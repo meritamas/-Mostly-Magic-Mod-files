77953ae1d360a1a92a7c0f0cafa140c006a58647
meritamas <87375070+meritamas@users.noreply.github.com>
2021-07-14 12:18:32 +0200

    Create MTMostlyMagicMod.cs

---
+++ Scripts/MTMostlyMagicMod.cs
@@ -0,0 +1,4 @@
+// Project:         MeriTamas's (Mostly) Magic Mod for Daggerfall Unity (http://www.dfworkshop.net)
+// Copyright:       Copyright (C) 2021 meritamas
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Author:          meritamas (meritamas@outlook.com)
